8a5e0f17eb406ca19843f25e63fad132ba709519
sakshinandgude6-web <sakshinandgude6@gmail.com>
2026-05-26 19:06:18 +0100

    Create expense model

--- backend/budgets/models.py
+++ backend/budgets/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Budget(models.Model):
+    limit = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    month = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.month
--- backend/expenses/models.py
+++ backend/expenses/models.py
@@ -7,3 +7,17 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+
+# Create Expense model
+class Expense(models.Model):
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    date = models.DateField()
+
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.amount)
