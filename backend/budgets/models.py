35d75ce72a018f2363b7e3b09a5175ffdd3d938f
sakshinandgude6-web <sakshinandgude6@gmail.com>
2026-05-27 17:51:53 +0100

    Update schema for all models

--- backend/budgets/models.py
+++ backend/budgets/models.py
@@ -1,13 +1,34 @@
+import uuid
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Budget(models.Model):
-    limit = models.DecimalField(
-        max_digits=10,
-        decimal_places=2
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
     )
 
-    month = models.CharField(max_length=20)
+    user = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    on_delete=models.CASCADE,
+    related_name="budgets",
+    )
+
+    limit_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    warning_threshold = models.IntegerField()
+
+    month = models.CharField(
+        max_length=10,
+    )
 
     def __str__(self):
-        return self.month
+        return (
+            f"{self.user} - {self.month}"
+        )
--- backend/expenses/models.py
+++ backend/expenses/models.py
@@ -1,23 +1,72 @@
 from django.db import models
-
+import uuid
+from django.conf import settings
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=100)
-    
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+
+    name = models.CharField(
+        max_length=100,
+    )
+
+    color = models.CharField(
+        max_length=20,
+    )
+
     def __str__(self):
         return self.name
 
 # Create Expense model
 class Expense(models.Model):
-    amount = models.DecimalField(
-        max_digits=10,
-        decimal_places=2
+
+    TYPE_CHOICES = [
+        ("expense", "Expense"),
+        ("income", "Income"),
+    ]
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
     )
 
-    date = models.DateField()
+    user = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    on_delete=models.CASCADE,
+    related_name="expenses",
+    )
 
-    type = models.CharField(max_length=100)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        related_name="expenses",
+    )
+
+    description = models.TextField()
+
+    expense_date = models.DateField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    type = models.CharField(
+        max_length=20,
+        choices=TYPE_CHOICES,
+    )
 
     def __str__(self):
-        return str(self.amount)
+        return (
+            f"{self.amount}"
+        )
